c200474edf9c6afbb06e71ba156eed4d9324aa5b
stenwie <stenwie91@hotmail.it>
2019-07-15 15:01:41 +0200

    add loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Wrapper from './Wrapper';
 
 export default class App extends Component {
 
   state = {
-    currentValue: '',
+    currentValue: '0,00',
     error: '',
     curTime:'',
+    loader: true,
   }
 
 
@@ -18,39 +20,44 @@ export default class App extends Component {
   }
 
   getRenderValue = () => {
-    fetch("https://blockchain.info/it/ticker")
-    .then(response => response.json())
-    .then(data => {
-        // you can access your data here
-        this.setState({
-          currentValue: data.EUR.last,
-          curTime : new Date().toLocaleString()
+    setTimeout(() => {
+      fetch("https://blockchain.info/it/ticker")
+      .then(response => response.json())
+      .then(data => {
+          // you can access your data here
+          this.setState({
+            currentValue: data.EUR.last,
+            curTime : new Date().toLocaleString(),
+            loader: false,
+          })
+          localStorage.setItem('currentValue', this.state.currentValue)
+          console.log(data)
         })
-        localStorage.setItem('currentValue', this.state.currentValue)
-        console.log(data)
-      })
-      .catch((err) => {
-        this.setState({
-          error: 'Unable to load Price', err,
-        })  
-        console.log('Unable to load Price', err);
-      });
+        .catch((err) => {
+          this.setState({
+            error: 'Unable to load Price', err,
+          })  
+          console.log('Unable to load Price', err);
+        });  
+    }, 1000);
   };
   
 
   render() {
-    const {error, currentValue, curTime} = this.state;
+    const {error, currentValue, curTime, loader} = this.state;
     const realValue = (error === '') ? currentValue : error;
     const prevValue = localStorage.getItem('currentValue')
     const lastUpdate = (error === '') ? curTime : '-';
+    const loading = (loader === true) ? <Wrapper /> : null;
     return (
       <div className="App">
           <h1>
             Bitcoin price
           </h1>
           {`${realValue} €`}
+          {loading}
           <p>
-          {`Difference respect previous value: ${[(realValue - prevValue) / prevValue ] * 100} %`}
+          {loader === false ? `Difference respect previous value: ${[(realValue - prevValue) / prevValue ] * 100} %` : null}
           </p>
           <p>
           {`Last update: ${lastUpdate}`}
