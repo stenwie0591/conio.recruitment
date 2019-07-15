746b1af24fa2b48783f009bf10b734ce54ef83e9
stenwie <stenwie91@hotmail.it>
2019-07-15 10:22:57 +0200

    get in console bitcoin data from fetch url

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class MuseumImgBanner extends Component {
 
-export default App;
+  componentDidMount() {
+        fetch("https://blockchain.info/it/ticker")
+        .then(response => response.json())
+        .then(data => {
+            // you can access your data here
+            console.log(data)
+          })
+          .catch((err) => {
+            console.log('Geolocation parsing failed', err);
+          });
+      }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
+}
